8bed8a141dcf60093a48015cb5b2d0128e664422
Hiro0808 <huxima0808@gmail.com>
2023-03-29 10:52:13 +0900

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -21,7 +21,7 @@ class User < ApplicationRecord
   validates :nickname, presence: true
   #validates :last_name#, presence: true
   #validates :first_name#, presence: true
- # validates :last_name_kana#, presence: true
+  #validates :last_name_kana#, presence: true
   #validates :first_name_kana#, presence: true
   validates :birthday, presence: true
 
