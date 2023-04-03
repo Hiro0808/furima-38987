31eed535b303d25312ed98d0ae3c2be699f006db
Hiro0808 <huxima0808@gmail.com>
2023-04-03 17:43:18 +0900

    ActiveHash導入
    ActiveHashの各モデル作成
    indexおよびnewのviewsファイル編集
    ルーティング設定
    itemsコントローラー設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,14 @@
 class Item < ApplicationRecord
   has_one_attached :image
 
+  
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :shipping_burden
+  belongs_to :prefecture
+  belongs_to :date_of_shipmen
+
+  validates :item_name, :description, :price, presence: true
+  validates :category_id, :condition_id, :shipping_burden_id, :prefecture_id, :date_of_shipment_id,numericality: { other_than: 1 ,message: "can't be blank"} 
 end
