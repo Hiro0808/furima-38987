b5eec6a85665685aa1503535bf80c7c02bc4fe48
Hiro0808 <huxima0808@gmail.com>
2023-04-13 18:52:02 +0900

    ja.yml　formオブジェクトに対応

--- app/models/item.rb
+++ app/models/item.rb
@@ -12,6 +12,6 @@ class Item < ApplicationRecord
   belongs_to :date_of_shipment
 
   validates :item_name, :description, :price, :image,presence: true
-  validates :category_id, :condition_id, :shipping_burden_id, :prefecture_id, :date_of_shipment_id,numericality: { other_than: 1 ,message: "can't be blank"} 
+  validates :category_id, :condition_id, :shipping_burden_id, :prefecture_id, :date_of_shipment_id,numericality: { other_than: 1 ,message: "を入力してください"} 
   validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, only_integer: true }
 end
